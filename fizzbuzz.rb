4c2870883513b4e217ff8542ab9c4c35592c7bcd
Jacqueline Lee <anginblu@gmail.com>
2017-10-05 16:09:16 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+puts "What's your number?"
+
+number = gets.strip
+
+def FixxBuzz (number = gets.strip.to_i)
+  if number/3.class == integer && number/5.class == integer
+    puts "FizzBuzz"
+  else if number/3.class == integer
+    puts "Fizz"
+  else if number/5.class == integer
+      puts "Buzz"
+end
+    
