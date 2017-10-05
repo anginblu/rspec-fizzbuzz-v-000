3c58f2a36eabf9241ad04ce2f4c4aa1f969dfe04
Jacqueline Lee <anginblu@gmail.com>
2017-10-05 16:26:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
 
-def FixxBuzz
+def FizzBuzz
   puts "What's your number?"
   number = gets.strip.to_i
 if number % 3 == 0 && umber % 5 == 0
@@ -10,3 +10,5 @@ if number % 3 == 0 && umber % 5 == 0
       puts "Buzz"
   else puts ""
 end
+
+FizzBuzz
