354a7ab6fb1e310a5eb990a40b20b016fd5f72d1
Jacqueline Lee <anginblu@gmail.com>
2017-10-05 16:30:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,13 @@
 def FizzBuzz (number)
 if number.to_i % 3 == 0 && number.to_i % 5 == 0
      "FizzBuzz"
+   end
   else if number.to_i % 3 == 0
      "Fizz"
+   end
   else if number.to_i % 5 == 0
        "Buzz"
+     end
   else ""
+  end
 end
